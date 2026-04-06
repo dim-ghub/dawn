0d01f6d442a53b0c3836f1db311f046c8e4b1926
dim <dim@local>
2026-04-05 21:12:57 -0500

    Add NetworkManager-openrc to dependencies

--- user_scripts/arch_setup_scripts/scripts/060_package_installation.sh
+++ user_scripts/arch_setup_scripts/scripts/060_package_installation.sh
@@ -45,7 +45,7 @@ declare -ar pkgs_filesystem=(
 
 # Group 7: Network & Internet
 declare -ar pkgs_network=(
-	"networkmanager" "iwd" "nm-connection-editor" "inetutils" "wget" "curl" "openssh" "firewalld" "vsftpd" "reflector" "bmon" "ethtool" "httrack" "wavemon" "firefox" "network-manager-applet"
+	"networkmanager" "iwd" "NetworkManager-openrc" "nm-connection-editor" "inetutils" "wget" "curl" "openssh" "firewalld" "vsftpd" "reflector" "bmon" "ethtool" "httrack" "wavemon" "firefox" "network-manager-applet"
 )
 
 # Group 8: Terminal & Shell
